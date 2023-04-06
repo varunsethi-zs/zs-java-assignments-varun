c297822f7f7fc5fbc7cd71de13d01cbfe60f8919
varunsethi-zs <varun.sethi@zopsmart.com>
2023-04-06 16:13:37 +0530

    assignment-1 | added the Systeminfo file

---
+++ src/main/java/com/zopsmart/assignment1/SystemInfo.java
@@ -0,0 +1,28 @@
+package com.zopsmart.assignment1;
+
+import java.io.File;
+
+public class SystemInfo {
+
+    public static void main(String []args)
+    {
+        File systemSpace= new File("/");
+
+        System.out.println("Hello World");
+        System.out.println("username = " + System.getProperty("user.name"));
+        System.out.println("directory = "+System.getProperty("user.dir"));
+
+        System.out.println("memory = " +Runtime.getRuntime().maxMemory());
+
+        System.out.println("System Cpu/Cores = " + Runtime.getRuntime().availableProcessors());
+
+        long totalCapacity = systemSpace.getTotalSpace();
+
+        System.out.println("Total Disc size : " + totalCapacity);
+
+        System.out.println("OS Build : " +System.getProperty("os.arch"));
+        System.out.println("OS Version : " +System.getProperty("os.version"));
+
+
+    }
+}
