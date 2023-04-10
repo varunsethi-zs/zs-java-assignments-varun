7c827d1a2bfa6ee12e0303f55b62999ad51b0605
varunsethi-zs <varun.sethi@zopsmart.com>
2023-04-10 12:13:21 +0530

    assignment-1 | added java docs Systeminfo file

--- src/main/java/com/zopsmart/assignment1/SystemInfo.java
+++ src/main/java/com/zopsmart/assignment1/SystemInfo.java
@@ -7,18 +7,17 @@ public class SystemInfo {
     public static void main(String []args)
     {
         File systemSpace= new File("/");
-
         System.out.println("Hello Varun");
 
-        /** system.get Property function to get username  and user directory */
-
+        /** system.get Property function to get username */
         String user =System.getProperty("user.name");
         System.out.println("username = " + user);
+
+        /** system.get Property function to get user directory */
         String directory=System.getProperty("user.dir");
         System.out.println("directory = "+directory);
 
         /** Runtime.getRuntime().maxMemory function to get memory of system */
-
         long memory =Runtime.getRuntime().maxMemory()/1024/1024/1024;
         System.out.println("memory = " +memory+"gb");
 
@@ -26,17 +25,17 @@ public class SystemInfo {
         int Cores=Runtime.getRuntime().availableProcessors();
         System.out.println("System Cpu/Cores = " + Cores);
 
-        /** systemSpace.getTotalSpace function  to get total disc space of system */
-
+        /** systemSpace.getTotalSpace function to get total disc space of system */
         long totalCapacity = systemSpace.getTotalSpace()/1024/1024/1024;
         System.out.println("Total Disc size : " + totalCapacity+"gb");
 
-        /** system.get Property function to os build   and os version */
-
+        /** system.get Property function to os build */
         String osbuild =System.getProperty("os.arch");
         System.out.println("OS Build : " +osbuild);
+
+        /** system.get Property function to os version */
         String osversion =System.getProperty("os.version");
         System.out.println("OS Version : " +osversion);
-
     }
 }
+
