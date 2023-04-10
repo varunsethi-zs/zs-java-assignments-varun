20319a776c79693a29a35f317f5d07f5266a7956
varunsethi-zs <varun.sethi@zopsmart.com>
2023-04-10 15:59:24 +0530

    assignment-1 | varun_sethi | Reformatted the code for Systeminfo file

--- src/main/java/com/zopsmart/assignment1/SystemInfo.java
+++ src/main/java/com/zopsmart/assignment1/SystemInfo.java
@@ -4,38 +4,37 @@ import java.io.File;
 
 public class SystemInfo {
 
-    public static void main(String []args)
-    {
-        File systemSpace= new File("/");
+    public static void main(String[] args) {
+        File systemSpace = new File("/");
         System.out.println("Hello Varun");
 
         /** system.get Property function to get username */
-        String user =System.getProperty("user.name");
+        String user = System.getProperty("user.name");
         System.out.println("username = " + user);
 
         /** system.get Property function to get user directory */
-        String directory=System.getProperty("user.dir");
-        System.out.println("directory = "+directory);
+        String directory = System.getProperty("user.dir");
+        System.out.println("directory = " + directory);
 
         /** Runtime.getRuntime().maxMemory function to get memory of system */
-        long memory =Runtime.getRuntime().maxMemory()/1024/1024/1024;
-        System.out.println("memory = " +memory+"gb");
+        long memory = Runtime.getRuntime().maxMemory() / 1024 / 1024 / 1024;
+        System.out.println("memory = " + memory + "gb");
 
         /** Runtime.getRuntime().availableProcessors function to get Cpu Cores */
-        int Cores=Runtime.getRuntime().availableProcessors();
+        int Cores = Runtime.getRuntime().availableProcessors();
         System.out.println("System Cpu/Cores = " + Cores);
 
         /** systemSpace.getTotalSpace function to get total disc space of system */
-        long totalCapacity = systemSpace.getTotalSpace()/1024/1024/1024;
-        System.out.println("Total Disc size : " + totalCapacity+"gb");
+        long totalCapacity = systemSpace.getTotalSpace() / 1024 / 1024 / 1024;
+        System.out.println("Total Disc size : " + totalCapacity + "gb");
 
         /** system.get Property function to os build */
-        String osbuild =System.getProperty("os.arch");
-        System.out.println("OS Build : " +osbuild);
+        String osBuild = System.getProperty("os.arch");
+        System.out.println("OS Build : " + osBuild);
 
         /** system.get Property function to Os version */
-        String osversion =System.getProperty("os.version");
-        System.out.println("OS Version : " +osversion);
+        String osVersion = System.getProperty("os.version");
+        System.out.println("OS Version : " + osVersion);
     }
 }
 
