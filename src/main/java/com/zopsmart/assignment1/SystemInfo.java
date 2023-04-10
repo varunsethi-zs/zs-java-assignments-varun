3be1f103431c34622d7e511b2156a4718d862f8f
varunsethi-zs <varun.sethi@zopsmart.com>
2023-04-10 11:24:09 +0530

    assignment-1 | added java docs

--- src/main/java/com/zopsmart/assignment1/SystemInfo.java
+++ src/main/java/com/zopsmart/assignment1/SystemInfo.java
@@ -9,20 +9,34 @@ public class SystemInfo {
         File systemSpace= new File("/");
 
         System.out.println("Hello Varun");
-        System.out.println("username = " + System.getProperty("user.name"));
-        System.out.println("directory = "+System.getProperty("user.dir"));
 
-        System.out.println("memory = " +Runtime.getRuntime().maxMemory());
+        /** system.get Property function to get username  and user directory */
 
-        System.out.println("System Cpu/Cores = " + Runtime.getRuntime().availableProcessors());
+        String user =System.getProperty("user.name");
+        System.out.println("username = " + user);
+        String directory=System.getProperty("user.dir");
+        System.out.println("directory = "+directory);
 
-        long totalCapacity = systemSpace.getTotalSpace();
+        /** Runtime.getRuntime().maxMemory function to get memory of system */
 
-        System.out.println("Total Disc size : " + totalCapacity);
+        long memory =Runtime.getRuntime().maxMemory()/1024/1024/1024;
+        System.out.println("memory = " +memory+"gb");
 
-        System.out.println("OS Build : " +System.getProperty("os.arch"));
-        System.out.println("OS Version : " +System.getProperty("os.version"));
+        /** Runtime.getRuntime().availableProcessors function to get Cpu Cores */
+        int Cores=Runtime.getRuntime().availableProcessors();
+        System.out.println("System Cpu/Cores = " + Cores);
 
+        /** systemSpace.getTotalSpace function  to get total disc space of system */
+
+        long totalCapacity = systemSpace.getTotalSpace()/1024/1024/1024;
+        System.out.println("Total Disc size : " + totalCapacity+"gb");
+
+        /** system.get Property function to os build   and os version */
+
+        String osbuild =System.getProperty("os.arch");
+        System.out.println("OS Build : " +osbuild);
+        String osversion =System.getProperty("os.version");
+        System.out.println("OS Version : " +osversion);
 
     }
 }
